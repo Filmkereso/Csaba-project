1fc3f410367196681c8d50c7b38a4f192f368aca
thecsabamago <57727822+thecsabamago@users.noreply.github.com>
2020-01-07 14:23:43 +0100

    Refaktorálás a program szépítése.

---
+++ src/script.js
@@ -0,0 +1,12 @@
+function megjelenit()
+{
+    let regi = document.getElementById('popup_gomb_szoveg');
+    let uj = document.getElementById('gombnyomas_utani_tartalom');
+
+    regi.style.display = 'none';
+    uj.style.display = 'block';
+}
+
+window.setTimeout(function() {
+    document.getElementById('popup-container').style.display = 'flex';
+}, 3000);
